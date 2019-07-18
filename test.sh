707245ea19f892e22335bf9b89c87081690c77c5
craff <raffalli@univ-savoie.fr>
2019-07-17 23:43:15 -1000

    cleaning test.sh

--- test.sh
+++ test.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 
+opts=--no-print-directory
 cd tests
 
-dune build calc.exe test.exe big_expr.exe hard.exe
+dune build $opts calc.exe test.exe big_expr.exe hard.exe
 
-dune exec -- ./hard.exe 100000
+echo testing a grammar with cache
+dune exec $opts -- ./hard.exe 100000
 
-dune exec -- ./big_expr.exe 5 4 4 | time dune exec ./calc.exe
+echo "testing the calculator (with grammar/combinator)"
+dune exec $opts -- ./big_expr.exe 5 4 4 | time dune exec $opts ./calc.exe > /dev/null
 
-dune exec ./test.exe
+echo "testing the calculator (with ocamlyacc)"
+dune exec $opts -- ./big_expr.exe 5 4 4 | time dune exec $opts ./calc_yacc/calc.exe > /dev/null
+
+echo general tests
+dune exec $opts ./test.exe
