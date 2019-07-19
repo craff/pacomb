1c705ccb983684d83359c625ef6b11a67d5dc3e9
craff <raffalli@univ-savoie.fr>
2019-07-18 20:58:25 -1000

    cleaning parserPpx.ml

--- ppx/parserPpx.ml
+++ ppx/parserPpx.ml
@@ -3,11 +3,15 @@ open Parsetree
 open Ast_mapper
 open Ast_helper
 open Longident
+open Location
 
 let grmod s = Exp.ident (Location.mknoloc (Ldot(Lident "Grammar",s)))
 let lxmod s = Exp.ident (Location.mknoloc (Ldot(Lident "Lex",s)))
 
 let unit_ = Exp.construct (Location.mknoloc (Lident "()")) None
+
+let app f x = Exp.apply f [(Nolabel, x)]
+let app2 f x y = Exp.apply f [(Nolabel, x);(Nolabel, y)]
 
 let str_to_parser str =
   let fn item = match item.pstr_desc with
@@ -20,25 +24,23 @@ let str_to_parser str =
          let lexify exp =
            match exp.pexp_desc with
            | Pexp_constant (Pconst_char _) ->
-              Exp.apply (grmod "term")
-                [(Nolabel,Exp.apply (lxmod "char") [(Nolabel, exp);(Nolabel, unit_)])]
+              app (grmod "term") (app2 (lxmod "char") exp unit_)
            | Pexp_constant (Pconst_string _) ->
-              Exp.apply (grmod "term")
-                [(Nolabel,Exp.apply (lxmod "string") [(Nolabel, exp);(Nolabel, unit_)])]
+              app (grmod "term") (app2 (lxmod "string") exp unit_)
            | Pexp_construct({txt = Lident "INT"; _}, None) ->
-              Exp.apply (grmod "term")
-                [(Nolabel,Exp.apply (lxmod "int") [(Nolabel, unit_)])]
+              app (grmod "term") (app (lxmod "int") unit_)
            | Pexp_construct({txt = Lident "FLOAT"; _}, None) ->
-              Exp.apply (grmod "term")
-                [(Nolabel,Exp.apply (lxmod "float") [(Nolabel, unit_)])]
+              app (grmod "term") (app (lxmod "float") unit_)
            | _ -> exp
          in
          let item e =  match e.pexp_desc with
-           | Pexp_construct({txt = Lident "::"; _},
-                            Some({pexp_desc = Pexp_tuple
-                                   [ {pexp_desc = Pexp_ident {txt = Lident name; loc}; _}
-                                   ; exp ]; _})) ->
-              (Pat.var {loc; txt = name},lexify exp)
+           | Pexp_construct
+               ( { txt = Lident "::"; _}
+               , Some({pexp_desc =
+                         Pexp_tuple
+                           [ {pexp_desc = Pexp_ident {txt = Lident name; loc}; _}
+                           ; exp ]; _})) ->
+              (Pat.var (mkloc name loc),lexify exp)
            | _ ->
               (Pat.any (), lexify e)
          in
@@ -49,25 +51,16 @@ let str_to_parser str =
            | _ -> [item e]
          in
          let rec rules e = match e.pexp_desc with
-           | Pexp_apply({ pexp_desc = Pexp_ident {txt = Lident "=>"; _}; _ },
-                        [(Nolabel,rule);(Nolabel,action)]) ->
+           | Pexp_apply
+             ( { pexp_desc = Pexp_ident {txt = Lident "=>"; _}; _ }
+             , [(Nolabel,rule);(Nolabel,action)]) ->
               let rule = items rule in
               let args, rule = List.split rule in
-              let fn exp name =
-                Exp.fun_ Nolabel None name exp
-              in
-              let action =
-                List.fold_left fn action args
-              in
-              let action =
-                Exp.apply (grmod "empty") [(Nolabel,action)]
-              in
-              let fn item exp =
-                Exp.apply (grmod "seqf") [(Nolabel,item);(Nolabel,exp)]
-              in
-              let rule =
-                List.fold_right fn rule action
-              in
+              let fn exp name = Exp.fun_ Nolabel None name exp in
+              let action = List.fold_left fn action args in
+              let action = app  (grmod "empty") action in
+              let fn item exp = app2 (grmod "seqf") item exp in
+              let rule = List.fold_right fn rule action in
               [rule]
            | Pexp_sequence(e1,e2) ->
               rules e1 @ rules e2
@@ -77,10 +70,8 @@ let str_to_parser str =
          (name,rules)
        in
        let rules = List.map gn ls in
-       let fail = Exp.apply (grmod "fail") [(Nolabel,unit_)] in
-       let fn rule exp =
-         Exp.apply (grmod "alt") [(Nolabel,rule);(Nolabel,exp)]
-       in
+       let fail = app (grmod "fail") unit_ in
+       let fn rule exp = app2 (grmod "alt") rule exp in
        List.map
          (fun (name, rules) -> (name, List.fold_right fn rules fail))
          rules
@@ -91,8 +82,7 @@ let str_to_parser str =
     let gn (name, _) =
       Str.value Nonrecursive
         [Vb.mk (Pat.var name)
-           (Exp.apply (grmod "declare_grammar")
-              [(Nolabel, Exp.constant (Const.string name.txt))])]
+           (app (grmod "declare_grammar") (Exp.constant (Const.string name.txt)))]
     in
     List.map gn l
   in
@@ -100,9 +90,9 @@ let str_to_parser str =
     let fn (name, rules) =
       Str.value Nonrecursive
         [Vb.mk (Pat.any ())
-           (Exp.apply (grmod "set_grammar")
-              [(Nolabel, Exp.ident (Location.mkloc (Lident name.txt) name.loc))
-              ;(Nolabel, rules)])]
+           (app2 (grmod "set_grammar")
+              (Exp.ident (Location.mkloc (Lident name.txt) name.loc))
+              rules)]
     in
     List.map fn l
   in
