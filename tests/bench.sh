dff99d153b29c2c1b3be91b17c2c194732f4f737
craff <raffalli@univ-savoie.fr>
2019-07-20 18:19:59 -1000

    added a script for benchmak (test are now fast)

---
+++ tests/bench.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+make
+
+opts=--no-print-directory
+cd tests
+
+dune build $opts calc.exe test.exe big_expr.exe hard.exe
+
+echo general tests
+dune exec $opts -- ./test.exe --catalan 15 --sequence 10000
+
+echo testing a grammar with cache
+dune exec $opts -- ./hard.exe 100000
+
+echo "testing the calculator (with grammar/combinator)"
+dune exec $opts -- ./big_expr.exe 5 4 4 | time dune exec $opts ./calc.exe > /dev/null
+
+echo "testing the calculator (with grammar/combinator, using fammilies for prio)"
+dune exec $opts -- ./big_expr.exe 5 4 4 | time dune exec $opts ./calc_prio.exe > /dev/null
+
+echo "testing the calculator (with ocamlyacc)"
+dune exec $opts -- ./big_expr.exe 5 4 4 | time dune exec $opts ./calc_yacc/calc.exe > /dev/null
