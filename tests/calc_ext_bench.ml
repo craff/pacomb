488c026ff85cfbaa5c2e43bbc81c52b28abb6015
craff <raffalli@univ-savoie.fr>
2019-07-27 17:29:29 -1000

    forgotten one test file

---
+++ tests/calc_ext_bench.ml
@@ -0,0 +1,60 @@
+open Pacomb
+open Lex
+open Pos
+open Grammar
+
+(* factorisation ... just  a test for the ppx,
+   here left factorisation is done by the elimination of left recursion,
+   to the result is the same as with calc_prio.ml *)
+
+let eps = 1e-10
+
+type assoc = RightAssoc | LeftAssoc | NonAssoc
+
+let prios =  [("^", (( ** ), 2.0, RightAssoc))
+             ;("*", (( *. ), 4.0, LeftAssoc))
+             ;("/", (( /. ),4.0, LeftAssoc))
+             ;("+", (( +. ),6.0, LeftAssoc))
+             ;("-", (( -. ),6.0, LeftAssoc))
+             ]
+
+let%parser bin = (c::RE("[-&~^+=*/\\$!:]+\\(_[a-zA-Z0-9_]+\\)?")) => c
+
+let%parser op pmin pmax =
+  (c::bin) =>
+    try let (f,p,a) = List.assoc c prios in
+        let good = match a with
+          | NonAssoc -> pmin < p && p < pmax
+          | LeftAssoc -> pmin <= p && p < pmax
+          | RightAssoc -> pmin < p && p <= pmax
+        in
+        if not good then give_up ();
+        (p,f)
+    with Not_found -> give_up ()
+
+let%parser rec
+  expr pmax = (r::dseqf (expr pmax) ~cs:(Charset.from_string "-&~^+=*/\\$!:")
+                    (fun pe ->
+                      dseqf (op pe pmax)
+                        ~cs:(Charset.from_string "-+0-9(")
+                        (fun pop -> seqf (expr pop)
+                           (empty (fun (_,e2) pop b _ e1 -> (pop, b e1 e2))))))
+                                                  => r
+            ; (x::FLOAT)                          => (0.0,x)
+            ; '(' (e::expr_top) ')'               => (0.0,e)
+
+and expr_top = ((__,e)::expr 1000.0) => e
+
+let blank = Lex.blank_charset (Charset.singleton ' ')
+
+let _ =
+  try
+    while true do
+      let f () =
+        Printf.printf "=> %!";
+        let line = input_line stdin in
+        Printf.printf "%f\n%!" (parse_string expr_top blank line )
+      in handle_exception f ()
+    done
+  with
+    End_of_file -> ()
