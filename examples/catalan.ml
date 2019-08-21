f15a5d492008fc6daddf64ea6cf35e5d5d91ae64
craff <raffalli@univ-savoie.fr>
2019-08-20 14:27:47 -1000

    modif in catalan

--- examples/catalan.ml
+++ examples/catalan.ml
@@ -11,6 +11,11 @@ type trees =
   | Uni of trees * trees * int (* union of two sets *)
   | Bin of trees * trees * int (* all binary nodes with one tree in each set *)
 
+let rec print_tree ch = function
+    Nil -> Printf.fprintf ch "()"
+  | Uni(t1,t2,_) -> Printf.fprintf ch "(%a+%a)" print_tree t1 print_tree t2
+  | Bin(t1,t2,_) -> Printf.fprintf ch "(%a*%a)" print_tree t1 print_tree t2
+
 (* the above type carries the cardinal of the set becaue computing it
    is hard as we have a lot of sharing *)
 let rec uni x y = Uni(x,y,nb_tree x + nb_tree y)
@@ -23,7 +28,7 @@ and nb_tree = function
 (* A parser parsing arbitrary sequence of charaters 'a' as a set of binary
    trees. We use merge in case of ambiguity and pacomb keep sharing when we
    parse twice the same part of the input (@merge implies @cache) *)
-let%parser rec bin_seq =
+let%parser [@merge uni] rec bin_seq =
     ()                             => Nil
   ; (t1::bin_seq) 'a' (t2::bin_seq) => bin t1 t2
 
@@ -52,13 +57,21 @@ let catalan_max =
     Printf.eprintf "usage: %s max_len\n%!" Sys.argv.(0);
     exit 1
 
+let parse_all_string g s =
+  let s = Input.from_string s in
+  Grammar.parse_all_buffer g Lex.noblank s 0
+
 let _ =
   Printf.printf "checking the number of parsetrees on an ambiguous example,\
                  using merge and cache\n%!";
   for i = 0 to catalan_max  do
     let s = String.make i 'a' in
     let t0 = Unix.gettimeofday () in
-    let t = Grammar.parse_string (Grammar.cache ~merge:uni bin_seq) Lex.noblank s in
+    let t = parse_all_string bin_seq s in
+    let t = match t with
+        [] -> assert false
+      | t::ts -> List.fold_left uni t ts
+    in
     let t1 = Unix.gettimeofday () in
     let j = nb_tree t in
     let k = catalan i in
