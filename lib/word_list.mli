56f0176cfe670459d1a66d91fcbc4523d62881cc
craff <raffalli@univ-savoie.fr>
2019-08-24 06:34:18 -1000

    cleaning comments in word_list.mli

--- lib/word_list.mli
+++ lib/word_list.mli
@@ -1,53 +1,49 @@
+(** {1 Module to build and parse list of words} *)
 
-(** type of a word list with
+(** Type of a word list with
     'a : the type of characters (typically, char or Uchar.t)
     'b : a value associated to each word *)
 type ('a,'b) t
 
-(** create a ne empty table *)
+(** Create a new empty table *)
 val create : unit -> ('a,'b) t
 
-(**returns the number of bindings in the table *)
+(** Returns the number of bindings in the table *)
 val size : ('a,'b) t -> int
 
-(** [add_ascii tbl s v] adds a binding from [s] to [v] in [tbl],
-    keep all previous bindings.
+(** [add_ascii  tbl s  v] adds  a binding  from [s]  to [v]  in [tbl],  keep all
+    previous bindings.
 
-    a [map] function transforming character before addition (typically
-    a case transformer) can be prvided (defaut to identity). *)
+    [map] is a function transforming character before addition (typically a case
+    transformer). (defaults to identity). *)
 val add_ascii : ?map:(char -> char)
                  -> (char,'b) t -> string -> 'b -> unit
 
-(** [replace_ascii tbl s v] adds a binding from [s] to [v] in [tbl],
-    remove all previous bindings *)
+(** [replace_ascii tbl s v] adds a binding  from [s] to [v] in [tbl], remove all
+    previous bindings *)
 val replace_ascii : ?map:(char -> char)
                      -> (char,'b) t -> string -> 'b -> unit
 
-(** [mem_ascii tbl s] tells if [s] if present in [tbl]. Typically used
-    to reject identifiers that are keywords *)
+(** [mem_ascii tbl s] tells if [s] if present in [tbl]. Typically used to reject
+    identifiers that are keywords *)
 val mem_ascii : ?map:(char -> char) -> (char,'b) t -> string -> bool
 
-(** same as above for an unicode string *)
+(** Same as above for an unicode string *)
 val add_utf8 : ?map:(Uchar.t -> Uchar.t)
                -> (Uchar.t, 'b) t -> string -> 'b -> unit
 val replace_utf8 : ?map:(Uchar.t -> Uchar.t)
                    -> (Uchar.t,'b) t -> string -> 'b -> unit
-
-(** [mem_utf8 tbl s] tells if [s] if present in [tbl]. Typically used
-    to reject identifiers that are keywords *)
 val mem_utf8 : ?map:(Uchar.t -> Uchar.t) -> (Uchar.t,'b) t -> string -> bool
 
-(** parses word from a dictionnary returning as action all
-    the assiociated values (it is an ambiguous grammar if
-    there is more than one value.
+(**  Parses word  from a  dictionnary returning  as action  all the  assiociated
+    values (it is an ambiguous grammar if there is more than one value).
 
-    [final_test] will be called after parsing. It may be used
-    typically to ensure that the next character is not alphanumeric.
-    Defaults to an always passing test.
+    [final_test]  will be  called after  parsing. It  may be  used typically  to
+    ensure that the  next character is not alphanumeric.  Defaults  to an always
+    passing test.
 
-    [map] is called on each character before searching in the table,
-    typically a case conversion. Defaults to identity.
- *)
+    [map] is called on each character before searching in the table, typically a
+    case conversion. Defaults to identity.  *)
 val word : ?final_test:(Input.buffer -> Input.pos -> bool)
            -> ?map:(char -> char) -> (char, 'a) t -> 'a Grammar.t
 
