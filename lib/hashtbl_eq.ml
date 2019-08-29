23c72759bf31c6aed3d26a96b2bb26692de1303f
craff <raffalli@univ-savoie.fr>
2019-08-28 23:58:25 -1000

    use Marshal instead of Obj to compare closures

--- lib/hashtbl_eq.ml
+++ lib/hashtbl_eq.ml
@@ -2,41 +2,10 @@
     that compares closures using [(==)] instead of failing. Note that equality
     testing is consequently not perfect. *)
 let eq_closure : type a. a -> a -> bool = fun v1 v2 ->
-  (* We remember encountered values in [eq_done] to handle cyclicity. *)
-  let eq_done : (Obj.t * Obj.t) list ref = ref [] in
-  let rec eq : Obj.t -> Obj.t -> bool = fun v1 v2 ->
-    (* Physical equality is tested first. *)
-    v1 == v2 ||
-    (* We then look at tags, and unfold potential forward blocks. *)
-    let t1 = Obj.tag v1 in
-    if t1 = Obj.forward_tag then eq (Obj.field v1 0) v2 else
-    let t2 = Obj.tag v2 in
-    if t2 = Obj.forward_tag then eq v1 (Obj.field v2 0) else
-    (* Tags must otherwise be the same to have equality. *)
-    t1 == t2 &&
-    (* Strings, doubles and arrays of doubles are compared using [=]. *)
-    if t1 = Obj.string_tag then v1 = v2 else
-    if t1 = Obj.double_tag then v1 = v2 else
-    if t1 = Obj.double_array_tag then v1 = v2 else
-    (* For everything that is not a non-constant constructors, equality failed
-    at this point (e.g., for closures or sealed values).  Such values are only
-    considered equal if physical equality succeeds (it was tested already). *)
-    Obj.first_non_constant_constructor_tag <= t1 &&
-    t1 <= Obj.last_non_constant_constructor_tag &&
-    Obj.size v1 == Obj.size v2 && (* Number of fields should be equal. *)
-    (* We recursively explore the fields. *)
-    let rec gn i =
-      i < 0 || (eq (Obj.field v1 i) (Obj.field v2 i) && gn (i-1))
-    in
-    List.exists (fun (v1', v2') -> v1 == v1' && v2 == v2') !eq_done ||
-    begin
-      List.for_all (fun (v1', v2') -> v1 != v1' && v2 != v2') !eq_done &&
-      (eq_done := (v1, v2) :: !eq_done; gn (Obj.size v1 - 1))
-    end
-  in
-  eq (Obj.repr v1) (Obj.repr v2)
+  try v1 = v2
+  with Invalid_argument _ ->
+    Marshal.(to_string v1 [Closures] = to_string v2 [Closures])
 
-(** Representation of a hash table (very standard). *)
 type ('a, 'b) t =
   { eq_key             : 'a -> 'a -> bool
   (** Equality function for keys. *)
