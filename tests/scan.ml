56e8dea14624dca817412b8f3bb912ea911892c5
craff <christophe@raffalli.eu>
2019-12-18 10:43:42 -1000

    cleaning tests/scan.ml

--- tests/scan.ml
+++ tests/scan.ml
@@ -2,13 +2,25 @@
 (* This test is for measuring the performance of Input *)
 open Pacomb
 
-let utf8 =
-  if Array.length Sys.argv = 3 && Sys.argv.(1) = "--utf8" then
-    Utf8.UTF8 else Utf8.ASCII
+let utf8 = ref Utf8.ASCII
 
-let buf = Input.from_file ~utf8 Sys.argv.(Array.length Sys.argv - 1)
+let files = ref []
 
-let _ =
+let spec = Arg.[("--utf8", Unit(fun () ->  utf8 := Utf8.UTF8), "use utf8 mode")]
+
+let _ = Arg.parse spec
+          (fun x -> files := x :: ! files)
+          "scan [--utf8] files ('-' to read stdin)"
+
+let utf8 = !utf8
+let files = if !files = [] then ["-"] else !files
+
+let do_file file =
+  let buf =
+    if file = "-" then
+      Input.from_fd ~utf8 Unix.stdin
+    else Input.from_file ~utf8 file
+  in
   let t0 = Unix.gettimeofday () in
   let rec fn n buf pos =
     let (c,buf,pos) = Input.read buf pos in
@@ -19,5 +31,8 @@ let _ =
   let d = t1 -. t0 in
   let s = (float) n /. d /. (1024. *. 1024.) in
   let m = (float n) /.  (1024. *. 1024.) in
-  Printf.printf "%.2f Mo/%d lines read in %f.3s (%.2f Mo/s) with utf8 mode %b\n"
-    m l d s (utf8 = Utf8.UTF8)
+  Printf.printf
+    "%s: %.2f Mo/%d lines read in %f.3s (%.2f Mo/s) with utf8 mode %b\n"
+    file m l d s (utf8 = Utf8.UTF8)
+
+let _ = List.iter do_file files
