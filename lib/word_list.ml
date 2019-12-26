7402f080555713b4f398fc192e5c82af59ba15ca
craff <christophe@raffalli.eu>
2019-12-26 05:51:54 -1000

    cleaning word_list. Options are given at table creation. Coorecteness of cs is tested when adding words

--- lib/word_list.ml
+++ lib/word_list.ml
@@ -3,15 +3,28 @@ module Pacomb = struct
   module Grammar = Grammar
 end
 
-type ('a,'b) t =
+type ('a,'b) data =
   { mutable leafs : 'b list
-  ; next  : ('a, ('a, 'b) t) Hashtbl.t }
+  ; next  : ('a, ('a, 'b) data) Hashtbl.t }
 
-let create () = { leafs = []; next = Hashtbl.create 8 }
+type ('a,'b) t =
+  { data : ('a, 'b) data
+  ; uniq : bool
+  ; map  : 'a -> 'a
+  ; cs   : Charset.t
+  ; finl : Input.buffer -> Input.pos -> bool }
 
-let reset t = t.leafs <- []; Hashtbl.clear t.next
+let create_data () = { leafs = []; next = Hashtbl.create 8 }
 
-let size {leafs; next} =
+let idt x = x
+
+let create ?(unique=true) ?(map=idt)
+      ?(cs=Charset.full) ?(final_test=fun _ _ -> true) () =
+  { data = create_data () ; uniq = unique; map; cs; finl = final_test }
+
+let reset t = t.data.leafs <- []; Hashtbl.clear t.data.next
+
+let size { data = {leafs; next}; _} =
   let res = ref 0 in
   let rec fn _ {leafs; next} =
     res := !res + List.length leafs;
@@ -23,23 +36,25 @@ let size {leafs; next} =
 
 type ('a,'b) fold = ('a -> 'b -> 'a) -> 'a -> 'a
 
-let add : bool -> ('a -> 'a) -> ('a,'b) t
-          -> (('a,'b) t, 'a) fold -> 'b -> unit =
-  fun repl map tbl fold v ->
-    let f tbl c =
+exception Already_bound
+
+let add : ('a,'b) t -> (('a,'b) data, 'a) fold -> 'b -> unit =
+  fun { data; uniq; map } fold v ->
+    let f data c =
       let c = map c in
       try
-        Hashtbl.find tbl.next c
+        Hashtbl.find data.next c
       with Not_found ->
-        let r = create () in
-        Hashtbl.add tbl.next c r;
+        let r = create_data () in
+        Hashtbl.add data.next c r;
         r
     in
-    let tbl = fold f tbl in
-    tbl.leafs <- if repl then [v] else v :: tbl.leafs
+    let data = fold f data in
+    if uniq && data.leafs <> [] then raise Already_bound;
+    data.leafs <- v :: data.leafs
 
-let mem : ('a -> 'a) -> ('a,'b) t -> (('a,'b) t, 'a) fold -> bool =
-  fun map tbl fold ->
+let mem : ('a,'b) t -> (('a,'b) data, 'a) fold -> bool =
+  fun { data = tbl; map; _ } fold ->
     let f tbl c =
       Hashtbl.find tbl.next (map c)
     in
@@ -49,85 +64,101 @@ let mem : ('a -> 'a) -> ('a,'b) t -> (('a,'b) t, 'a) fold -> bool =
     with
       Not_found -> false
 
-let idt x = x
-
-let mem_ascii : ?map:(char -> char) -> (char,'b) t -> string -> bool =
-  fun ?(map=idt) tbl s ->
+let mem_ascii : (char,'b) t -> string -> bool =
+  fun tbl s ->
     let fold f a =
       let res = ref a in
       String.iter (fun c -> res := f !res c) s;
       !res
     in
-    mem map tbl fold
+    mem tbl fold
 
-let add_ascii : bool -> (char -> char) -> (char,'b) t -> string -> 'b -> unit =
-  fun repl map tbl s v ->
+let add_ascii : (char,'b) t -> string -> 'b -> unit =
+  fun tbl s v ->
+    if s = "" then invalid_arg "Word_list.add: empty word";
+    if not (Charset.mem tbl.cs s.[0]) then
+      invalid_arg "Word_list.add: charset mismatch";
     let fold f a =
       let res = ref a in
       String.iter (fun c -> res := f !res c) s;
       !res
     in
-    add repl map tbl fold v
+    add tbl fold v
 
-let replace_ascii ?(map=idt) tbl s v = add_ascii true map tbl s v
-let add_ascii     ?(map=idt) tbl s v = add_ascii false map tbl s v
+let mem_utf8 : (string, 'b) t -> string -> bool =
+  fun tbl s ->
+    mem tbl (fun f a -> Utf8.fold_grapheme f a s)
 
-let mem_utf8 : ?map:(string -> string)
-               -> (string, 'b) t -> string -> bool =
-  fun ?(map=idt) tbl s ->
-    mem map tbl (fun f a -> Utf8.fold_grapheme f a s)
+let add_utf8 : (string, 'b) t -> string -> 'b -> unit =
+  fun tbl s v ->
+    add tbl (fun f a -> Utf8.fold_grapheme f a s) v
 
-let add_utf8 : bool -> (string -> string)
-               -> (string, 'b) t -> string -> 'b -> unit =
-  fun repl map tbl s v ->
-    add repl map tbl (fun f a -> Utf8.fold_grapheme f a s) v
+let next tbl c = Hashtbl.find tbl.next c
 
-let replace_utf8 ?(map=idt) tbl s v = add_utf8 true map tbl s v
-let add_utf8     ?(map=idt) tbl s v = add_utf8 false map tbl s v
-
-let next tbl c =
-  try Hashtbl.find tbl.next c with Not_found -> raise Lex.NoParse
-
-let word : ?name:string -> ?cs:Charset.t
-           -> ?final_test:(Input.buffer -> Input.pos -> bool)
-           -> ?map:(char -> char) -> (char, 'a) t -> 'a Grammar.t =
-  fun ?name ?(cs=Charset.full)
-      ?(final_test=fun _ _ -> true)
-      ?(map=fun c -> c) tbl ->
-    let rec f tbl s n =
-      try
-        let (c,s,n) = Input.read s n in
-        let c = map c in
-        f (next tbl c) s n
-      with
-        Not_found ->
-        if final_test s n && tbl.leafs <> [] then (tbl.leafs, s, n)
-        else raise Lex.NoParse
-    in
+let word : ?name:string -> (char, 'a) t -> 'a Grammar.t =
+  fun ?name { data = tbl; map; cs; finl; uniq } ->
     let n = Lex.default "WORD" name in
-    let f = f tbl in
-    let lex = Lex.{ n; f; a = Custom(f,Assoc.new_key ()); c = cs }
-    in
-    Grammar.unmerge ?name (Grammar.term lex)
-
-let utf8_word : ?name:string -> ?cs:Charset.t
-                -> ?final_test:(Input.buffer -> Input.pos -> bool)
-                -> ?map:(string -> string) -> (string, 'a) t -> 'a Grammar.t =
-  fun ?name ?(cs=Charset.full)
-      ?(final_test=fun _ _ -> true)
-      ?(map=fun c -> c) tbl ->
-    let rec f tbl s n =
-      try
-        let (c,s,n) = Lex.((any_grapheme ()).f s n) in
-        let c = map c in
-        f (next tbl c) s n
-      with
-        Not_found ->
-        if final_test s n && tbl.leafs <> [] then(tbl.leafs, s, n)
+    if uniq then
+      let rec f tbl s n =
+        try
+          let (c,s,n) = Input.read s n in
+          let c = map c in
+          f (next tbl c) s n
+        with
+          Not_found ->
+          if finl s n && tbl.leafs <> [] then (List.hd tbl.leafs, s, n)
+          else raise Lex.NoParse
+      in
+      let f = f tbl in
+      let lex = Lex.{ n; f; a = Custom(f,Assoc.new_key ()); c = cs }
+      in
+      Grammar.term ?name lex
+    else
+      let rec f tbl s n =
+        try
+          let (c,s,n) = Input.read s n in
+          let c = map c in
+          f (next tbl c) s n
+        with
+          Not_found ->
+          if finl s n && tbl.leafs <> [] then (tbl.leafs, s, n)
         else raise Lex.NoParse
-    in
-    let n = Lex.default "WORD" name in
-    let f = f tbl in
-    let lex = Lex.{ n; f; a = Custom(f,Assoc.new_key ()); c = cs }
-    in
-    Grammar.unmerge ?name (Grammar.term lex)
+      in
+      let f = f tbl in
+      let lex = Lex.{ n; f; a = Custom(f,Assoc.new_key ()); c = cs }
+      in
+      Grammar.unmerge ?name (Grammar.term lex)
+
+let utf8_word : ?name:string -> (string, 'a) t -> 'a Grammar.t =
+  fun ?name { data = tbl; map; finl; cs; uniq } ->
+    let n = Lex.default "UTF8_WORD" name in
+    if uniq then
+      let rec f tbl s n =
+        try
+          let (c,s,n) = Lex.((any_grapheme ()).f s n) in
+          let c = map c in
+          f (next tbl c) s n
+        with
+          Not_found ->
+          if finl s n && tbl.leafs <> [] then(List.hd tbl.leafs, s, n)
+          else raise Lex.NoParse
+      in
+      let f = f tbl in
+      let lex = Lex.{ n; f; a = Custom(f,Assoc.new_key ()); c = cs }
+      in
+      Grammar.term ?name lex
+    else
+      let rec f tbl s n =
+        try
+          let (c,s,n) = Lex.((any_grapheme ()).f s n) in
+          let c = map c in
+          f (next tbl c) s n
+        with
+          Not_found ->
+          if finl s n && tbl.leafs <> [] then (tbl.leafs, s, n)
+          else raise Lex.NoParse
+      in
+      let f = f tbl in
+      let lex = Lex.{ n; f; a = Custom(f,Assoc.new_key ()); c = cs }
+      in
+      Grammar.unmerge ?name (Grammar.term lex)
--- lib/word_list.mli
+++ lib/word_list.mli
@@ -5,8 +5,28 @@
     'b : a value associated to each word *)
 type ('a,'b) t
 
-(** Create a new empty table *)
-val create : unit -> ('a,'b) t
+(** exception raise when multiple binding are added and not allowed *)
+exception Already_bound
+
+(** Create  a new  empty table. The  optional parameter [  unique ]  defaults to
+   true. Setting  it to false  with allow multiple identical  bindings, creating
+   ambiguous grammars. If [ unique ] is true, then adding multiple bindings will
+   raise the exception [ Already_bound ].
+
+   [map] is a function transforming  character before addition (typically a case
+   transformer or a unicode normalisation). (defaults to identity).
+
+   [final_test]  will be  called  after parsing.  It may  be  used typically  to
+   ensure that the next character is not alphanumeric.  Defaults to an always
+   passing test.
+
+   [cs]  can be  given as  an optimisation.  All words  added should  start with
+   characters in this set. *)
+val create : ?unique:bool ->
+             ?map:('a -> 'a) ->
+             ?cs:Charset.t ->
+             ?final_test:(Input.buffer -> Input.pos -> bool)
+             -> unit -> ('a,'b) t
 
 (** Returns the number of bindings in the table *)
 val size : ('a,'b) t -> int
@@ -15,41 +35,21 @@ val size : ('a,'b) t -> int
 val reset : ('a,'b) t -> unit
 
 (** [add_ascii  tbl s  v] adds  a binding  from [s]  to [v]  in [tbl],  keep all
-    previous bindings.
+    previous bindings. *)
 
-    [map] is a function transforming character before addition (typically a case
-    transformer). (defaults to identity). *)
-val add_ascii : ?map:(char -> char) -> (char,'b) t -> string -> 'b -> unit
-
-(** [replace_ascii tbl s v] adds a binding  from [s] to [v] in [tbl], remove all
-    previous bindings *)
-val replace_ascii : ?map:(char -> char) -> (char,'b) t -> string -> 'b -> unit
+val add_ascii : (char,'b) t -> string -> 'b -> unit
 
 (** [mem_ascii tbl s] tells if [s] if present in [tbl]. Typically used to reject
     identifiers that are keywords *)
-val mem_ascii : ?map:(char -> char) -> (char,'b) t -> string -> bool
+val mem_ascii : (char,'b) t -> string -> bool
 
-(** Same as above for an unicode string, which are splitted in graphemes *)
-val add_utf8
-  : ?map:(string -> string) -> (string, 'b) t -> string -> 'b -> unit
-val replace_utf8
-  : ?map:(string -> string) -> (string,'b) t -> string -> 'b -> unit
-val mem_utf8
-  : ?map:(string -> string) -> (string,'b) t -> string -> bool
+(** Same as above for a unicode string, which are splitted in graphemes *)
+val add_utf8 : (string, 'b) t -> string -> 'b -> unit
+
+val mem_utf8 : (string,'b) t -> string -> bool
 
 (**  Parses word  from a  dictionnary returning  as action  all the  assiociated
-    values (it is an ambiguous grammar if there is more than one value).
+    values (it is an ambiguous grammar if there is more than one value).*)
+val word : ?name:string -> (char, 'a) t -> 'a Grammar.t
 
-    [final_test]  will be  called after  parsing. It  may be  used typically  to
-    ensure that the  next character is not alphanumeric.  Defaults  to an always
-    passing test.
-
-    [map] is called on each character before searching in the table, typically a
-    case conversion. Defaults to identity.  *)
-val word : ?name:string -> ?cs:Charset.t
-           -> ?final_test:(Input.buffer -> Input.pos -> bool)
-           -> ?map:(char -> char) -> (char, 'a) t -> 'a Grammar.t
-
-val utf8_word : ?name:string -> ?cs:Charset.t
-                -> ?final_test:(Input.buffer -> Input.pos -> bool)
-                -> ?map:(string -> string) -> (string, 'a) t -> 'a Grammar.t
+val utf8_word : ?name:string -> (string, 'a) t -> 'a Grammar.t
