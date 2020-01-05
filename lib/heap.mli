bc2f1aabe0e64c0fa6c2ab5b3ea742e6299310be
craff <christophe@raffalli.eu>
2020-01-05 07:44:58 -1000

    comments in heap.mli

--- lib/heap.mli
+++ lib/heap.mli
@@ -8,6 +8,6 @@ val empty : 'a t
 (** [add cmp x h] add [x] to the heap [h] with the priority [p] *)
 val add : ('a -> 'a -> int) -> 'a -> 'a t -> 'a t
 
-(** [remove h] return an element with the least priority from the heap and the
-     resulting heap. Sema        ntics is unspecified for equal priorities *)
+(** [remove h] returns an element with  the least priority from the heap and the
+   resulting heap. Semantics is unspecified for equal priorities *)
 val remove : 'a t -> ('a * 'a t)
