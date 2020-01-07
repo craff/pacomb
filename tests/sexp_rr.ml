222ad4c90cf367820a827a8cc27fe013b6efbd45
craff <christophe@raffalli.eu>
2020-01-06 22:32:19 -1000

    use lazy in sexp_rr.ml

--- tests/sexp_rr.ml
+++ tests/sexp_rr.ml
@@ -15,9 +15,9 @@ let rec size e = match e.e with
 let id = "[a-zA-Z_][a-zA-Z_0-9]*[']*"
 
 let%parser rec sexp = (x::RE id)   => { l = x_lpos; r = x_rpos; e = Idt x }
-              ; '(' (l::sexps) ')' => { l = l_lpos; r = l_rpos; e = Lst l }
-and sexps = () => []
-           ; (e::sexp) (l::sexps) => e::l
+              ; '(' (lazy l::sexps) ')' => { l = l_lpos; r = l_rpos; e = Lst l }
+and sexps = () => lazy []
+           ; (e::sexp) (lazy l::sexps) => lazy (e::l)
 
 let blank = Blank.from_charset (Charset.from_string " \t\n\r")
 
